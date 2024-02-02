162d6c44a24c6753705500fecc5a254036907bfa
ericsalesdeandrade <sdaeric19@gmail.com>
2024-02-02 12:53:55 +0000

    Readme

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Freezegun Example Code
-This repo contains the sample code for the article - **A Simple Guide To Controlling Time in Pytest using Freezegun**
+# Pytest Ignore Directory Example Code
+This repo contains the sample code for the article - [3 Simple Ways To Ignore Test Directories in Pytest](https://pytest-with-eric.com/getting-started/pytest-ignore-directory/)
 
 # Requirements
-* Python (3.10.6)
+* Python (3.11+)
 
 Please install the dependencies via the `requirements.txt` file using 
 ```bash
@@ -13,7 +13,7 @@ If you don't have Pip installed, please follow instructions online on how to do 
 # How To Run the Unit Tests
 To run the Unit Tests from the root of the repo, run
 ```bash
-pytest -v
+pytest
 ```
 
 If you have any questions about the project, please raise an Issue on GitHub. 
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,23 +1,33 @@
-from src.converter import centimeter_to_meter, meter_to_centimeter, foot_to_inch, inch_to_foot
+from src.converter import (
+    centimeter_to_meter,
+    meter_to_centimeter,
+    foot_to_inch,
+    inch_to_foot,
+)
 import pytest
+
 
 def test_centimeter_to_meter():
     assert centimeter_to_meter(100) == 1
 
+
 def test_meter_to_centimeter():
     assert meter_to_centimeter(1) == 100
+
 
 def test_foot_to_inch():
     assert foot_to_inch(1) == 12
 
+
 def test_inch_to_foot():
     assert inch_to_foot(12) == 1
 
-@pytest.mark.skip(reason="We didn't completed millimeter_to_centimeter() function")
+
+@pytest.mark.skip(reason="Function not implemented yet")
 def test_millimeter_to_centimeter():
     assert millimeter_to_centimeter(10) == 1
 
 
-@pytest.mark.xfail(reason="We didn't create any centimeter_to_millimeter() function. So, we're declaring this test as FAIL")
+@pytest.mark.xfail(reason="Function expected to fail")
 def test_centimeter_to_millimeter():
     assert centimeter_to_millimeter(10) == 1
